bfa2b420b32ec7da5bd6d80292354e7e31a66480
David Lu <uldivad9@gmail.com>
2015-04-12 06:01:31 +0900

    kancolle distraction level >9001

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 var placeControlPanel = function() {
-	$(document.body).append("<div id=\"controlpanel\"></div>");
+	$(document.body).prepend("<div id=\"controlpanel\"></div>");
 	$("#controlpanel").append("<center><div id=\"cp_div_username\"></div><div id=\"cp_div_buttons\"</div></center>");
 	$("#cp_div_username").append("<span id=\"username\">usernamegoeshere</span>");
 	$("#cp_div_buttons").append("<span id=\"cp_button1\" class=\"ui-icon ui-icon-person\"></span>");
@@ -8,13 +8,13 @@ var placeControlPanel = function() {
 }
 
 var placeMiniSearchBar = function() {
-	$(document.body).append("<div id=\"minisearchbar\"></div>");
+	$(document.body).prepend("<div id=\"minisearchbar\"></div>");
 	$("#minisearchbar").append("<center><div id=\"logo\"></div><div id=\"inputbox\"></div></center>");
 	$("#logo").append("<img id=\"logo_main\" src=\"assets/logo_main.png\" width=\"180px\" height=\"60px\" alt=\"Checkmate.gg logo\">");
-	$("#inputbox").append("<form id=\"searchform\"><input id=\"searchinput\" type=\"text\" size=\"80\"/></form>");
+	$("#inputbox").append("<form id=\"searchform\"><input id=\"searchfield\" type=\"text\" size=\"80\"/></form>");
 }
 
 var placeTopOfPage = function() {
-	placeMiniSearchBar();
 	placeControlPanel();
+	placeMiniSearchBar();
 }
