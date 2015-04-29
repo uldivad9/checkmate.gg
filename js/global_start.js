6d12a4d3b590a1421428ada7c7305497ea20a9db
Dai Yang <zephyred@mit.edu>
2015-04-29 19:53:46 -0400

    Magnifying glass image

--- js/global_start.js
+++ js/global_start.js
@@ -33,6 +33,7 @@ var placeMiniSearchBar = function() {
 	$(document.body).prepend("<div id=\"minisearchbar\"></div>");
 	$("#minisearchbar").append("<center><div id=\"logo\"></div><div id=\"inputbox\"></div></center>");
 	$("#logo").append("<a href=\"./main.html\"><img id=\"logo_main\" src=\"assets/logo_main.png\" width=\"180px\" height=\"60px\" alt=\"Checkmate.gg logo\"></a>");
+	$("#logo").append("<a href=\"./main.html\"><img id=\"mani_glass\" src=\"assets/magni_glass.png\" width=\"180px\" height=\"60px\" alt=\"Checkmate.gg logo\"></a>");
 	$("#inputbox").append("<form id=\"searchform\" action=\"./results.html\" method=\"get\"><input id=\"searchfield\" type=\"text\" name=\"tags\" size=\"80\"/><button type=\"submit\" style=\"display: none;\"></form>");
 	$("#searchfield").select2({tags: ['caro-kann', 'opening', 'endgame', 'midgame'], multiple: true, width: '60%', openOnEnter: false});
 	/*$("#searchform").submit(function() {
